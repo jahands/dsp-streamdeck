9a2a622431cee854366f4c382d1386484e43674c
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-07-18 22:02:47 +0200

    create basic code structure

--- ProductionPublisher/ProductionPublisher.cs
+++ ProductionPublisher/ProductionPublisher.cs
@@ -1,33 +1,65 @@
 ﻿using System;
+using System.Net;
 using BepInEx;
 using HarmonyLib;
 using UnityEngine;
+using WebSockets;
 
 namespace ProductionView
 {
     [BepInPlugin("com.github.jahands.dsp_production", Strings.Name, Strings.Version)]
     public class ProductionPublisherMod : BaseUnityPlugin
     {
+        private const int TickLimit = 10;
+        private static ProductionPublisherMod _instance;
+        private static int _count;
         private Harmony _harmony;
+        private WebSocketServer _socket;
 
         public void Awake()
         {
+            _instance = this;
             _harmony = new Harmony(typeof(ProductionPublisherMod).FullName);
+            _socket = new WebSocketServer();
+            _socket.Connected += SocketConnected;
+        }
+
+        public void Reset()
+        {
+            _instance = null;
+            _harmony.UnpatchAll();
+            _harmony = null;
+            _socket.Dispose();
+            _socket = null;
         }
 
         public void Start()
         {
             _harmony.PatchAll(typeof(ProductionPublisherMod));
+            _socket.Bind(new IPEndPoint(IPAddress.Any, 1320));
+            _socket.StartAccept();
             ProductionPublisherDebug.Log("Started!");
         }
 
-        public void Update()
+        private void SocketConnected(object sender, WebSocket e)
         {
+            // todo Handle incoming sockets
         }
 
-        public void Reset()
+        [HarmonyPatch(typeof(ProductionStatistics), "AfterTick")]
+        [HarmonyPostfix]
+        public static void StatisticsPatch(ProductionStatistics __instance)
         {
-            _harmony.UnpatchAll();
+            if (++_count > TickLimit && __instance != null)
+            {
+                _count = 0;
+                _instance.StatisticsUpdateTick(__instance);
+            }
+        }
+
+        public void StatisticsUpdateTick(ProductionStatistics it)
+        {
+            // todo Publish ONLY required data to WebSocket
         }
     }
 
